aced64de83b665f38a27bc33e12ba9fca29274d9
sakoPO <sakopo@sakopo.sakopo>
2024-02-18 02:00:01 +0100

    Updated paths.

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,4 +1,4 @@
 from capacitors_generator import generate_capacitors
 
 
-generate_capacitors("../parts/capacitors", "../script_data_sources")
+generate_capacitors("../components/capacitors", "../script_data_sources")
