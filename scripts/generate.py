4f79f7100e715383b19d63dc74512b5e556e314c
sakoPO <sakopo@sakopo.sakopo>
2024-03-03 19:19:32 +0100

    Updated generator script.

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,5 +1,5 @@
 from capacitors_generator import generate_capacitors
 from resistors_generator import generate_resistors
 
-generate_capacitors("../components/capacitors", "../script_data_sources")
+generate_capacitors("../components/capacitors", "../script_data_sources/capacitors")
 generate_resistors("../components/resistors", "../script_data_sources")
