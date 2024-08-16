ab9b21443c6ce4dc4aaa9072fda0358d9393c32e
sakoPO <sakopo@sakopo.sakopo>
2024-08-16 13:45:33 +0200

    Added schematic symbol generator.

---
+++ scripts/schematic_symbol_generator/main.py
@@ -0,0 +1,42 @@
+import json
+
+from pathlib import Path
+from part_library_gen import symbol_generator
+
+
+def load_files(directory):
+    files = Path(directory).rglob('*.json')
+    return [x for x in files if not str(x).endswith('_generated.json')]
+
+
+def process_part_data(part):
+    if "symbol&footprint" in part:
+        symbol_footprint = part["symbol&footprint"]
+        if 'pinmap' in symbol_footprint and len(symbol_footprint['pinmap']) > 0:
+            print("Processing", part['partNumber'])
+            symbol_data = {
+                'designator': "U?",
+                'manufacturer': part['manufacturer'],
+                'part': part['partNumber'],
+                'pins': symbol_footprint['pinmap'],
+                'symbol_generator': {'default': {}}
+            }
+            if 'symbol_generator' in symbol_footprint:
+                print("Using dedicated generator")
+                for generator in symbol_footprint['symbol_generator']:
+                    symbol_data['symbol_generator'] = {generator: symbol_footprint['symbol_generator'][generator]}
+                    symbol_generator.generate(symbol_data)
+            else:
+                symbol_generator.generate(symbol_data)
+
+
+def generate_schematic_symbols(files):
+    for file in files:
+        with open(file, 'r') as f:
+            data = json.load(f)
+            for part in data:
+                process_part_data(part)
+
+
+if __name__ == "__main__":
+    generate_schematic_symbols(load_files("components"))
